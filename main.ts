c6d302cb66d6ce97e4a636c7044973de1dcb8074
daido1976 <daido1976@gmail.com>
2022-10-05 19:42:52 +0900

    genStmts works single table

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-import { Parser } from "https://esm.sh/@dbml/core@2.4.2";
+import { genStmts } from "./generator.ts";
 
 const rawDbml = Deno.readFileSync("./sample.dbml");
 const decoder = new TextDecoder("utf-8");
 const dbml = decoder.decode(rawDbml);
 
-const db = Parser.parse(dbml, "dbml");
-console.log(db);
+const stmts = genStmts(dbml);
+console.log(stmts);
