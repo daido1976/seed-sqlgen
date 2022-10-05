6665dead2c8cdeee8113512ec42b61e00f379c90
daido1976 <daido1976@gmail.com>
2022-10-05 19:51:25 +0900

    genStmts works multiple table

--- generator.test.ts
+++ generator.test.ts
@@ -5,16 +5,6 @@ import { genStmts } from "./generator.ts";
 describe("genStmts", () => {
   it("works single table", () => {
     const input =
-      "Table suppliers {\n  id int [pk, increment]\n  name varchar\n}\n\n";
-    const expected = [
-      "INSERT INTO `suppliers` (`name`) VALUES ('suppliers_name_1');",
-    ];
-    const actual = genStmts(input);
-    assertEquals(actual, expected);
-  });
-
-  it("works single table", () => {
-    const input =
       "Table suppliers {\n  id int [pk, increment]\n  name varchar\n age int\n}\n\n";
     const expected = [
       "INSERT INTO `suppliers` (`name`, `age`) VALUES ('suppliers_name_1', 1);",
@@ -23,14 +13,14 @@ describe("genStmts", () => {
     assertEquals(actual, expected);
   });
 
-  // it("works multiple table", () => {
-  //   const input =
-  //     "Project scoutea {\n  database_type: 'MySQL'\n  Note: 'scoutea db'\n}\n\nTable suppliers {\n  id int [pk, increment]\n  name varchar\n}\n\nTable teas {\n  id int [pk, increment]\n  name varchar\n  stock int\n  price float\n  supplier_id int\n}\n";
-  //   const expected = [
-  //     "INSERT INTO `suppliers` (`name`) VALUES ('suppliers_name_1');",
-  //     "INSERT INTO `teas` (`name`, `stock`, `rate`, `supplier_id`) VALUES ('teas_name_1', 1, 0.5, 1);",
-  //   ];
-  //   const actual = genStmts(input);
-  //   assertEquals(actual, expected);
-  // });
+  it("works multiple table", () => {
+    const input =
+      "Project scoutea {\n  database_type: 'MySQL'\n  Note: 'scoutea db'\n}\n\nTable suppliers {\n  id int [pk, increment]\n  name varchar\n}\n\nTable teas {\n  id int [pk, increment]\n  name varchar\n  stock int\n  rate float\n  supplier_id int\n}\n";
+    const expected = [
+      "INSERT INTO `suppliers` (`name`) VALUES ('suppliers_name_1');",
+      "INSERT INTO `teas` (`name`, `stock`, `rate`, `supplier_id`) VALUES ('teas_name_1', 1, 0.5, 1);",
+    ];
+    const actual = genStmts(input);
+    assertEquals(actual, expected);
+  });
 });
--- generator.ts
+++ generator.ts
@@ -2,42 +2,44 @@ import { Parser } from "https://esm.sh/@dbml/core@2.4.2";
 
 export const genStmts = (dbml: string): string[] => {
   const db = Parser.parse(dbml, "dbml");
-  const table = db.schemas[0].tables[0];
-  const v = table.fields.reduce(
-    (acc, field) => {
-      if (field.increment) {
+  return db.schemas[0].tables.map((table) => {
+    const v = table.fields.reduce(
+      (acc, field) => {
+        if (field.increment) {
+          return acc;
+        }
+
+        const colStr = `\`${field.name}\``;
+        acc.columnsStr =
+          acc.columnsStr === "" ? colStr : acc.columnsStr.concat(", ", colStr);
+
+        const colType: string = field.type.type_name;
+        let valStr = "";
+        switch (colType) {
+          case "varchar":
+            valStr = `'${singularize(field.table.name)}_${field.name}_1'`;
+            break;
+          case "int":
+            valStr = "1";
+            break;
+          case "float":
+            valStr = "0.5";
+            break;
+        }
+
+        acc.valuesStr =
+          acc.valuesStr === "" ? valStr : acc.valuesStr.concat(", ", valStr);
         return acc;
+      },
+      {
+        // e.g. "`name`, `stock`, `rate`, `supplier_id`"
+        columnsStr: "",
+        // e.g. "'teas_name_1', 1, 0.5, 1"
+        valuesStr: "",
       }
-
-      const colStr = `\`${field.name}\``;
-      acc.columnsStr =
-        acc.columnsStr === "" ? colStr : acc.columnsStr.concat(", ", colStr);
-
-      const colType: string = field.type.type_name;
-      let valStr = "";
-      switch (colType) {
-        case "varchar":
-          valStr = `'${singularize(field.table.name)}_${field.name}_1'`;
-          break;
-        case "int":
-          valStr = "1";
-          break;
-        case "float":
-          valStr = "0.5";
-          break;
-      }
-
-      acc.valuesStr =
-        acc.valuesStr === "" ? valStr : acc.valuesStr.concat(", ", valStr);
-      return acc;
-    },
-    {
-      columnsStr: "",
-      valuesStr: "",
-    }
-  );
-  const stmt = `INSERT INTO \`${table.name}\` (${v.columnsStr}) VALUES (${v.valuesStr});`;
-  return [stmt];
+    );
+    return `INSERT INTO \`${table.name}\` (${v.columnsStr}) VALUES (${v.valuesStr});`;
+  });
 };
 
 // TODO: 実装する
