fd17a4fd54ff044ad8bec34f30041eef5211beb5
daido1976 <daido1976@gmail.com>
2022-10-06 06:59:22 +0900

    Fix misc

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ import { genStmts } from "./generator.ts";
 import { parse } from "https://deno.land/std@0.158.0/flags/mod.ts";
 
 const args = parse(Deno.args);
-const inputFile = args.in ?? "./sample.dbml";
-const outFile: string | boolean | null = args.o || null;
+// -in <inputfile>
+const inputFile: string = args.in || "./sample.dbml";
+// -o or -o <outfile>
+const outFile: string | boolean = args.o || false;
 const dbml = Deno.readTextFileSync(inputFile);
 const stmts = genStmts(dbml);
 
