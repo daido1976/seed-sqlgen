1f87f8c26247e9797142e19065487f708dd76c11
daido1976 <daido1976@gmail.com>
2022-10-13 09:42:55 +0900

    Rename sample db

--- generator.test.ts
+++ generator.test.ts
@@ -15,7 +15,7 @@ describe("genStmts", () => {
 
   it("works multiple table", () => {
     const input =
-      "Project scoutea {\n  database_type: 'MySQL'\n  Note: 'scoutea db'\n}\n\nTable suppliers {\n  id int [pk, increment]\n  name varchar\n}\n\nTable teas {\n  id int [pk, increment]\n  name varchar\n  stock int\n  rate float\n expiration_date date\n expired boolean\n supplier_id int\n}\n";
+      "Project sample {\n  database_type: 'MySQL'\n  Note: 'sample db'\n}\n\nTable suppliers {\n  id int [pk, increment]\n  name varchar\n}\n\nTable teas {\n  id int [pk, increment]\n  name varchar\n  stock int\n  rate float\n expiration_date date\n expired boolean\n supplier_id int\n}\n";
     const expected = [
       "INSERT INTO `suppliers` (`name`) VALUES ('suppliers_name_1');",
       "INSERT INTO `teas` (`name`, `stock`, `rate`, `expiration_date`, `expired`, `supplier_id`) VALUES ('teas_name_1', 1, 0.5, '2022-10-12', false, 1);",
